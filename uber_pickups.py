4c61819e3a3e62bfcb81f1ec91564bbc5c47de3d
aman Gupta <97275831+Amanraj24@users.noreply.github.com>
2022-08-21 20:15:47 +0530

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -52,3 +52,22 @@ elif option=='Content-Based Recommender System':
      out2=merged_left[merged_left["genres"]==ge ].sort_values(by=["genres","rating","userId"], ascending=False)
      out2=out2.reset_index(drop=True).title.head(int(rec))
      st.write(out2)
+else:
+     usr=st.text_input("UserID:",1)
+     rec=st.text_input("Num recommendations(N):",10)
+     thr=st.text_input("Threshold for similar users (k):",100)
+     usr_mv=merged_left[merged_left["userId"]==int(usr)][["title","userId"]]
+     a=usr_mv.title.tolist()
+     diff_usr=merged_left[merged_left["title"]==a[0]]
+     for i in range(1,len(a)):
+          diff_usr=diff_usr.append(merged_left[merged_left["title"]==a[i]])
+     diff_usr=diff_usr["userId"].unique()
+     diff_usr=diff_usr.tolist()
+     diff_usr=diff_usr[:int(thr)]
+     sg_mv=merged_left[merged_left["userId"]==diff_usr[0]]
+     for i in range(1,len(diff_usr)):
+          sg_mv=sg_mv.append(merged_left[merged_left["userId"]==diff_usr[i]])
+     sg_mv=sg_mv.reset_index(drop=True)
+     sg_mv=sg_mv["title"].unique()
+     sg_mv=pd.DataFrame(sg_mv,columns=["Movie Title"])
+     st.write(sg_mv.head(int(rec)))
