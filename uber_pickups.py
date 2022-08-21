0463e4c3656e07480826358099155015cb595b68
aman Gupta <97275831+Amanraj24@users.noreply.github.com>
2022-08-21 18:42:15 +0530

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -39,8 +39,8 @@ elif option=='Content-Based Recommender System':
           Remove_last = Str[:l-7]
           movies["title"][i]=Remove_last
      n_movies=movies
-     mv=st.text_input("Movie Title (t): ")
-     rec=st.text_input("Num recommendations (N):")
+     mv=st.text_input("Movie Title (t): ","Jumanji")
+     rec=st.text_input("Num recommendations (N):",5)
      movie=n_movies[n_movies["title"]==mv]
      
      id1=movie["movieId"].tolist()
@@ -52,35 +52,6 @@ elif option=='Content-Based Recommender System':
      out2=merged_left[merged_left["genres"]==ge ].sort_values(by=["genres","rating","userId"], ascending=False)
      out2=out2.reset_index(drop=True).title.head(int(rec))
      st.write(out2)
+else:
+     
 
-
-#DATE_COLUMN = 'date/time'
-#DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-#@st.cache
-#def load_data(nrows):
-#   data = pd.read_csv(DATA_URL, nrows=nrows)
-#    lowercase = lambda x: str(x).lower()
-#    data.rename(lowercase, axis='columns', inplace=True)
-#    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#    return data
-#
-#data_load_state = st.text('Loading data...')
-#data = load_data(10000)
-#data_load_state.text("Done! (using st.cache)")
-
-#if st.checkbox('Show raw data'):
-#    st.subheader('Raw data')
-#    st.write(data)
-
-#st.subheader('Number of pickups by hour')
-#hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-#st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-#hour_to_filter = st.slider('hour', 0, 23, 17)
-#filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-#st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-#st.map(filtered_data)
