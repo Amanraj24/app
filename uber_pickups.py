33c9ccb39c2e4910163e64210252f0ef66b1ce71
aman Gupta <97275831+Amanraj24@users.noreply.github.com>
2022-08-21 17:50:41 +0530

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -45,6 +45,7 @@ elif option=='Content-Based Recommender System':
      
      id1=movie["movieId"].tolist()
      id2=st.text(id1[0])
+     st.write(merged_left)
      genre=merged_left[merged_left["movieId"]==id2]
      #genre=genre["genres"]
      st.write(genre)
